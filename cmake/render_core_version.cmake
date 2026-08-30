c95b82e79b629709bf7dbf2dfd45a59d18ea4c52
ShiXiaoPaoYa <xpao_std@qq.com>
2026-08-31 04:30:50 +0800

    fix: keep CMake project version numeric

--- cmake/render_core_version.cmake
+++ cmake/render_core_version.cmake
@@ -4,5 +4,7 @@
 
 set(JELLYFRAME_RENDER_CORE_PACKAGE_VERSION "0.6.2-dev" CACHE STRING
     "Render Core package version")
+set(JELLYFRAME_RENDER_CORE_CMAKE_VERSION "0.6.2" CACHE STRING
+    "Numeric CMake project version for the Render Core package")
 set(JELLYFRAME_RENDER_CORE_ENGINE_ABI "1" CACHE STRING
     "Render Core public engine ABI version")
